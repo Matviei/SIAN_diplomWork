b255e80ae86b37e8c0c270be657703930268e9c2
Матвей Сунцов <matviei1109@yandex.ru>
2022-06-08 11:46:51 +0300

    Добавление стиля для кнопок и страницы сотрудников

---
+++ SIAN/Services/EmployeeChartCtor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using SIAN.Models;
+
+namespace SIAN.Services
+{
+    internal class EmployeeChartCtor
+    {
+        public static SeriesCollection SeriesCollect { get; set; }
+
+        public static SeriesCollection ChartEmployee(Employee selectedEmployee)
+        {
+
+            var TaskSubdd = DB_connect.db_connect.db.TaskSchedule.Where(c => c.ID_employee == selectedEmployee.ID_employee);
+            SeriesCollect = new SeriesCollection();
+            foreach (var lev in TaskSubdd)
+            {
+                SeriesCollect.Add(new ColumnSeries()
+                {
+                    Title = lev.Description,
+                    Values = new ChartValues<ObservableValue> { new ObservableValue(Convert.ToDouble(lev.SpentTime)) },
+                    DataLabels = true
+                });
+            }
+
+            return SeriesCollect;
+        }
+    }
+}
--- SIAN/Views/ProjectManagerViews/EmployeePage.xaml.cs
+++ SIAN/Views/ProjectManagerViews/EmployeePage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.RightsManagement;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LiveCharts;
+using SIAN.Models;
+using SIAN.Services;
 
 namespace SIAN.Views.ProjectManagerViews
 {
@@ -20,11 +24,53 @@ namespace SIAN.Views.ProjectManagerViews
     /// </summary>
     public partial class EmployeePage : Page
     {
+        private SeriesCollection _seriesCollection;
+
+        public SeriesCollection SeriesCollect
+        {
+            get
+            {
+                return _seriesCollection;
+            }
+            set
+            {
+                _seriesCollection = value;
+            }
+        }
         public EmployeePage()
         {
             InitializeComponent();
             LVEmployee.ItemsSource = DB_connect.db_connect.db.Employee.ToList();
-            CB.ItemsSource = DB_connect.db_connect.db.Status.ToList();
+            CBStatus.ItemsSource = DB_connect.db_connect.db.Status.ToList();
+            
+        }
+        
+        private void LVEmployee_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+           Sort();
+        }
+
+        public void Sort()
+        {
+            var select = LVEmployee.SelectedItem as Employee;
+            _seriesCollection = EmployeeChartCtor.ChartEmployee(select);
+            var cbList = DB_connect.db_connect.db.TaskSchedule
+                .Where(c => c.ID_employee == select.ID_employee).ToList();
+            TaskSheduleChart.Series = _seriesCollection;
+            if (CBStatus.SelectedIndex > 0)
+            {
+                var selectCB = (CBStatus.SelectedItem as Status).ID_status;
+                if (select != null)
+                    cbList =
+                        cbList.Where(c =>
+                            c.ID_status == selectCB).ToList();
+            }
+
+            LVTaskShedule.ItemsSource = cbList;
+        }
+        private void CBStatus_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Sort();
         }
     }
 }
